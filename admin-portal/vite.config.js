605c935a59e56ab550a8045db91886fdd08939fd
SaiSowjanya9 <saisowjanya218@gmail.com>
2026-08-13 16:26:21 -0500

    Revert: Remove manualChunks from vite config

--- admin-portal/vite.config.js
+++ admin-portal/vite.config.js
@@ -15,16 +15,5 @@ export default defineConfig({
         changeOrigin: true
       }
     }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    }
   }
 })
